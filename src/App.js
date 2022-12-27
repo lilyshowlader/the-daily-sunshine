b4602a647f6b55dbd7958e29590d757793c0236c
lilyshowlader <lilyshowlader@gmail.com>
2022-12-26 20:51:18 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
   
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=596039e2c61ba9dc2c0bdb9bb1974245`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=596039e2c61ba9dc2c0bdb9bb1974245`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -21,41 +21,43 @@ function App() {
   }
 
   return (
+    <div className='mobile'>
     <div className="container-one">
-      <div className='search'>
-        <input
-        value={location}
-        onChange={event => setLocation(event.target.value)} 
-        placeholder= 'enter location'
-        onKeyPress={searchLocation}
-        type='text'>
-        </input>
-      </div>
-
       <div className="item-one">
-        <p> the <br></br> daily <br></br>sunshine.</p>
+        <p className='app_name'> the <br></br> daily <br></br>sunshine.</p>
       </div>
 
       <div className='item-two'>
-
-          <div className='location'>
-            <p>{data.name}</p> 
-          </div>
-          <div className='temperature'>
-            {data.main ? <p>{data.main.temp}</p> : null}
-          </div>
-          <div className='description'>
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div> 
-          <div className='feels'>
-            {data.main ? <p>{data.main.feels_like}</p> : null}
-          </div>
-          <div className='humidity'> 
-            {data.main ? <p>{data.main.humidity}%</p> : null}
-          </div>
-          <div className='wind'>
-            {data.wind ? <p>{data.wind.speed}mph</p> : null}
-          </div>
+        <p>enter your location.</p>
+      {/* <div className='search'> */}
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)} 
+        placeholder= 'queens, nyc'
+        onKeyPress={searchLocation}
+        type='text'>
+        </input>
+      {/* </div> */}
+          {/* <div className='location'> */}
+            <h3>{data.name}</h3> 
+          {/* </div>
+          <div className='description'> */}
+            {data.weather ? <h5>{data.weather[0].main}</h5> : null}
+          {/* </div>
+          <div className='temperature'> */}
+            {data.main ? <h5>temp: {data.main.temp}°F</h5> : null}
+          {/* </div> */}
+  
+          {/* <div className='feels'> */}
+            {data.main ? <h5>feels like: {data.main.feels_like.toFixed()}°F</h5> : null}
+          {/* </div>
+          <div className='humidity'>  */}
+            {data.main ? <h5>humidity: {data.main.humidity}%</h5> : null}
+          {/* </div>
+          <div className='wind'> */}
+            {data.wind ? <h5>wind: {data.wind.speed.toFixed()}mph</h5> : null}
+          {/* </div> */}
+      </div>
       </div>
   </div>
 
