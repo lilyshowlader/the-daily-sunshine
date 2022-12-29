989acd8b7136721ecc95468dc56874667397c6b1
lilyshowlader <lilyshowlader@gmail.com>
2022-12-29 13:11:39 -0500

    remove index css, clean up mobile view

--- src/App.js
+++ src/App.js
@@ -6,29 +6,42 @@ import { useState } from 'react';
 
 function App() {
   const [data, setData] = useState({})
+  // why did we set this to an empty object?
   const [location, setLocation] = useState('')
-  
+  // this was set to a empty string because the user will update the string
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=596039e2c61ba9dc2c0bdb9bb1974245`
+  // this is the url for the API call. the location was replaced with a template literal of location because that is what will change dynamically based on user input 
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
+      // look into event.key
     axios.get(url).then((response) =>{
+      // go to the given url and when that is completed, take the response and set the data using setData to the response
       setData(response.data)
       console.log(response.data)
+      console.log(event)
     })
     setLocation('')
+    console.log(setLocation)
+    // the above line of code resets the input back to an empty string after a search is initiated
     }
   }
+
+  var dateWithoutSecond = new Date();
+
 
   return (
     <div className='mobile'>
     <div className="container-one">
+
       <div className="item-one">
+        <p className='time_and_date'>{dateWithoutSecond.toLocaleTimeString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})} </p>
         <p className='app_name'> the <br></br> daily <br></br>sunshine.</p>
       </div>
 
       <div className='item-two'>
-        <p>enter your location.</p>
+        <p className='location_enter'>enter your location.</p> 
         <input
         value={location}
         onChange={event => setLocation(event.target.value)} 
@@ -36,8 +49,8 @@ function App() {
         onKeyPress={searchLocation}
         type='text'>
         </input>
-          <h3>{data.name}</h3> 
-          {data.weather ? <h5>{data.weather[0].main}</h5> : null}
+          <h3>{data.name}</h3>
+          {data.weather ? <h5>{data.weather[0].description}</h5> : null}
           {data.main ? <h5>temp: {data.main.temp.toFixed()}°F</h5> : null}
           {data.main ? <h5>high: {data.main.temp_max.toFixed()}°F</h5> : null}{data.main ? <h5>low: {data.main.temp_min.toFixed()}°F</h5> : null}
           {data.main ? <h5>feels like: {data.main.feels_like.toFixed()}°F</h5> : null}
--- src/services /api-calls.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios';
-
-// const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=596039e2c61ba9dc2c0bdb9bb1974245'
-
-// // info that I want weatherMain, weatherDescription, weatherIcon, mainTemp, mainFeelsLike, mainMin, mainMax, mainHumidity, 
-
-// const searchWeather = async ()
-
-
